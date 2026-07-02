f425c8f73cc9d7e1038b97c67bb60be46343cf84
unknown <imhyrongyun365@gmail.com>
2026-07-02 13:50:29 +0900

    nav-button, 인재채용 페이지 수정

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -1,7 +1,7 @@
-import PageHero from '../components/common/PageHero'
-import SectionBlock from '../components/common/SectionBlock'
-import SubNav from '../components/navigation/SubNav'
-import { subNavItems } from '../data/subNavData'
+import PageHero from "../components/common/PageHero";
+import SectionBlock from "../components/common/SectionBlock";
+import SubNav from "../components/navigation/SubNav";
+import { subNavItems } from "../data/subNavData";
 
 function Support() {
   return (
@@ -12,7 +12,7 @@ function Support() {
         <SectionBlock key={item.id} id={item.id} title={item.label} />
       ))}
     </div>
-  )
+  );
 }
 
-export default Support
+export default Support;
